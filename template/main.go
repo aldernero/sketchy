57082d78831c1381f0792d33143e9502bf7f6959
Vernon Miller <vernon.w.miller@gmail.com>
2023-02-11 20:36:07 -0700

    added pprof flag, improved boilerplate

--- template/main.go
+++ template/main.go
@@ -4,13 +4,22 @@ import (
 	"flag"
 	"github.com/tdewolff/canvas"
 	"log"
+	"os"
+	"runtime/pprof"
 
 	"github.com/aldernero/sketchy"
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+func setup(s *sketchy.Sketch) {
+	// Setup logic goes here
+}
+
 func update(s *sketchy.Sketch) {
 	// Update logic goes here
+	//if s.DidControlsChange {
+	//	setup(s)
+	//}
 }
 
 func draw(s *sketchy.Sketch, c *canvas.Context) {
@@ -21,10 +30,19 @@ func main() {
 	var configFile string
 	var prefix string
 	var randomSeed int64
+	var cpuprofile = flag.String("pprof", "", "Collect CPU profile")
 	flag.StringVar(&configFile, "c", "sketch.json", "Sketch config file")
 	flag.StringVar(&prefix, "p", "", "Output file prefix")
 	flag.Int64Var(&randomSeed, "s", 0, "Random number generator seed")
 	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
 	s, err := sketchy.NewSketchFromFile(configFile)
 	if err != nil {
 		log.Fatal(err)
@@ -36,6 +54,7 @@ func main() {
 	s.Updater = update
 	s.Drawer = draw
 	s.Init()
+	setup(s)
 	ebiten.SetWindowSize(int(s.ControlWidth+s.SketchWidth), int(s.SketchHeight))
 	ebiten.SetWindowTitle("Sketchy - " + s.Title)
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeDisabled)
